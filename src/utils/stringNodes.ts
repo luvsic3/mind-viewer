6381dcb0994cde5256e257420c9aca4dc7d2f731
luvsic3 <1021679588@qq.com>
2021-01-31 22:54:31 +0800

    chore: set up preact

--- src/utils/stringNodes.ts
+++ src/utils/stringNodes.ts
@@ -1,5 +1,4 @@
 import FontInfo from './fontInfo'
-import { createCanvas } from 'canvas'
 
 interface FontNode {
   content?: string,
@@ -50,8 +49,7 @@ function getTextSize(node: FontNode) {
   const fontFamily = node.style.fontFamily
   const fontArr = [fontStyle, fontWeight, fontSizePx, fontFamily]
 
-  // const canvas = document.createElement('canvas')
-  const canvas = createCanvas(200, 200)
+  const canvas = document.createElement('canvas')
   const ctx = canvas.getContext('2d')
   ctx.font = fontArr.filter(item => item).join(' ')
 
